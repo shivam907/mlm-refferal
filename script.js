2050f45f3d472b7db99d59e36d2c2deb4c6af6e5
shivam907 <shivamkaushal907@gmail.com>
2023-01-28 21:19:38 +0530

    Made the Home Page Responsive

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const menu = document.querySelector(".navbar");
+console.log(menu);
+menu.addEventListener("click", (el) => {
+  document.querySelector(".navigation").classList.remove("none");
+  console.log("vadia");
+});
+const close = document.querySelector(".close");
+console.log("kida");
+close.addEventListener("click", () => {
+  document.querySelector(".navigation").classList.add("none");
+});
+
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".nav");
@@ -8,6 +20,10 @@ $(function () {
       "nav-scroll",
       $(this).scrollTop() > $nav.height() / 3
     );
-    // $(".logo").toggleClass("chitta", $(this).scrollTop() > $nav.height() / 3);
+    $(".navbar ion-icon").toggleClass(
+      "chitta",
+      $(this).scrollTop() > $nav.height() / 3
+    );
+    $(".logo").toggleClass("chitta", $(this).scrollTop() > $nav.height() / 3);
   });
 });
