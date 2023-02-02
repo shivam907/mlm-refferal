ca7551f459e306ffd941463cee0e01f71225ebeb
shivam907 <shivamkaushal907@gmail.com>
2023-02-02 23:13:50 +0530

    Done with transaction history and withdraw page

--- transactionHistory/script.js
+++ transactionHistory/script.js
@@ -5,3 +5,14 @@ document.onkeydown = () => {
   document.querySelector(".popUp").classList.add("none");
   console.log("ok");
 };
+const menu = document.querySelector(".navbar");
+console.log(menu);
+menu.addEventListener("click", (el) => {
+  document.querySelector(".navigation").classList.remove("none");
+  console.log("vadia");
+});
+const close = document.querySelector(".close");
+console.log("kida");
+close.addEventListener("click", () => {
+  document.querySelector(".navigation").classList.add("none");
+});
